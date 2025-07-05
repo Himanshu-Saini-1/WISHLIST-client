2af16ff1db98cda10964a77354915289bee8a678
HIMANSHUDCRUST <117287565+HIMANSHUDCRUST@users.noreply.github.com>
2025-07-05 16:32:47 +0530

    asd

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL || "http://localhost:5000"}/api`,
+  baseURL: `${process.env.REACT_APP_API_URL}`,
 });
 
 export const signup = (userData) => API.post("/users/signup", userData);
